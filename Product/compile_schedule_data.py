8b345b1974ec54dc24471c12d1f533b54ee88e17
hairyex5000 <you@example.com>
2024-05-11 20:08:34 -0700

    started laying the groundwork, no need for R

---
+++ Product/compile_schedule_data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df = pd.read_csv("schedule.csv")
+
+# for index, row in df.iterrows():
+#     while ()
+
+# need week date ranges to build web scraper
---
+++ Product/get_schedule_csv.r
@@ -0,0 +1,3 @@
+library(nflfastR)
+
+write.csv(fast_scraper_schedules(2000:2023), "schedule.csv")
