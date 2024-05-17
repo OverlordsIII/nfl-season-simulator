02f005b2931ee8c96faf19fa8e33e972753fc188
OverlordsIII <shrihunreddy@outlook.com>
2024-05-16 22:22:26 -0700

    use spencer's link grabbing method instead of manually typing out each link

--- Product/query_stats_weekly.py
+++ Product/query_stats_weekly.py
@@ -1,6 +1,6 @@
+import io
+from bs4 import BeautifulSoup as soup
 import pandas as pd
-
-stats = ["points-per-game", 'average-scoring-margin', 'yards-per-point', 'points-per-play']
 
 df = pd.read_csv("week_dates.csv")
 
@@ -8,12 +8,21 @@ dfSeasons = df.groupby('season').last()
 dfSeasons = dfSeasons.iloc[3:]
 print(dfSeasons)
 
-for stat in stats:
-    print(stat)
+links = []
+base = "https://www.teamrankings.com"
+
+# getting links from file
+with io.open("data_urls.html", "r") as a:
+    html = soup(a.read(), "html.parser")
+    for i in html.find_all('a'):
+        links.append(base + i.get('href', '/'))
+
+for link in links:
+    print(link)
     # gonna comment out for now - testing purposes
     # for index, row in dfSeasons.iterrows():
     row = dfSeasons.iloc[-1] # just use 2023 stats for now, can use other seasons later
-    dfPpg = pd.read_html("https://www.teamrankings.com/nfl/stat/" + stat + "?date=" + row['end'])[0]
+    dfPpg = pd.read_html(link + "?date=" + row['end'])[0]
     print(dfPpg)
 
 
