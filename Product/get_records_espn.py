06f315f67b16adb7950666973a591f68d8ec1155
hairyex5000 <87248345+hairyex5000@users.noreply.github.com>
2024-05-20 19:57:29 -0700

    Finished getting espn records

--- Product/get_records_espn.py
+++ Product/get_records_espn.py
@@ -16,13 +16,16 @@ for i in range(len(years)):
     years[i] = start + i
 
 for k in years:
-    df = pd.read_html(getURL(k))[::2]
+    df = pd.read_html(getURL(k))
+    df = list(filter(lambda x: "PF" in x.columns, df))
     temp = pd.DataFrame()
     print(df)
+    
     for j in df:
         j = j.rename(columns={j.columns.to_list()[0]: "team"})
         j.insert(0, column="Year", value=([k] * len(j)))
         temp = pd.concat([temp, j])
     all = pd.concat([all, temp])
 
+all = all[["Year", "team", "%"]]
 all.to_csv("records.csv")
