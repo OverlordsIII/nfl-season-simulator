1217d8ce081beed5e97b19a795c285e6c581d446
OverlordsIII <shrihunreddy@outlook.com>
2024-05-20 17:27:31 -0700

    get season and week

--- Product/query_stats_weekly.py
+++ Product/query_stats_weekly.py
@@ -25,10 +25,19 @@ j = len(endDates) * len(links)
 def getstuff(link, endDate):
     statDf = pd.read_html(link + "?date=" + endDate)[0]
     statDf = statDf[[statDf.columns[1], statDf.columns[2]]]
- #   temp = df[df['end'] == endDate][0][["season", "week"]]
-#    for index, row in statDf.iterrows():
- #       temptwo = temp.tolist().append([row["team"], str(link).split("/")[-1], row[temp[0]["season"]], row[temp[1]["season"]]])
- #       statList.loc[-1] = temptwo
+
+    temp2 = df[df['end'] == endDate]
+    season = temp2['season'][0]
+    week = temp2['week'][0]
+
+   # for index, row in statDf.iterrows():
+   #    global season
+   #    global week
+   #    global link
+   #    temptwo = [season, week].append([row["team"], str(link).split("/")[-1], row.iloc[-1], row[0])
+   #    statList.loc[-1] = temptwo
+
+
 for endDate in endDates:
     for link in links:
         print(str(i) + "/" + str(j))
