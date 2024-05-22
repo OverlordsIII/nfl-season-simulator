3515784a76a4d91bad37e070902a3e3412185cc8
hairyex5000 <87248345+hairyex5000@users.noreply.github.com>
2024-05-22 14:29:50 -0700

    Working on compiling

--- Product/compile_complete_season_data.py
+++ Product/compile_complete_season_data.py
@@ -13,4 +13,14 @@ for item in os.listdir("yearly_data/stats"):
 stats = stats.drop([stats.columns.to_list()[0]], axis=1)
 stats = stats.pivot_table(values='data', index=["season", "team"], columns="stat", aggfunc="first")
 
+records = records.drop([records.columns.to_list()[0]], axis=1)
+records = records.rename(columns={"Year": "season"})
+records = records.set_index(["season", "team"])
+
+for index, row in records.iterrows():
+    print(index)
+
+for index, row in stats.iterrows():
+
+    # stats.at[index, 'percent'] = records.at[index, '%']
 stats.to_csv("temp.csv")
