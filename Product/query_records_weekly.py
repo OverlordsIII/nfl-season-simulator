db55380e15e8bc1b35e03c7d6bae60a5255585d1
hairyex5000 <87248345+hairyex5000@users.noreply.github.com>
2024-05-29 13:45:42 -0700

    procedural decomposition of record and stat querying

---
+++ Product/query_records_weekly.py
@@ -0,0 +1,40 @@
+import pandas as pd
+
+def getRecords(weeks, years):
+    def getURL(year, week):
+        base = "https://shrpsports.com/nfl/stand.php?link=Y&season="
+        middle = "&divcnf=cnf&week=%20"
+        return ("%s%s%s%s" % (base, year, middle, week))
+
+    all = pd.DataFrame()
+
+    for k in years:
+        for i in weeks:
+            df = pd.read_html(getURL(k, i))
+            df = df[3:5]
+            temp = pd.DataFrame()
+            for j in df:
+                j = j.drop(0)
+                j = j.drop(1)
+                j.columns = [x for x in range(len(j.columns))]
+                j = j.rename(columns={2: "percent", 0: "team"})                
+                for index in j.index:
+                    elements = j['team'][index].split(" ")
+                    if (elements.__contains__("Chargers") or elements.__contains__("Rams")):
+                        elements = "LA " + elements[-1]
+                    elif (elements.__contains__("Giants") or elements.__contains__("Jets")):
+                        elements = "NY " + elements[-1]
+                    elif (elements.__contains__("Oakland")):
+                        elements = "Las Vegas"
+                    else:
+                        elements = " ".join(elements)
+                    j.at[index, 'team'] = elements
+                j.insert(0, column="season", value=([k] * len(j)))
+                j.insert(1, column="week", value=([i] * len(j)))
+                temp = pd.concat([temp, j])
+                all = pd.concat([all, temp])
+
+    all = all[["season", "week", "team", "percent"]]
+    return all
+
+getRecords(list(range(3,15)), [2003]).to_csv("yearly_data\stats\weekly\data_2003.csv")
--- Product/query_stats_weekly.py
+++ Product/query_stats_weekly.py
@@ -2,59 +2,58 @@ import io
 from bs4 import BeautifulSoup as soup
 import pandas as pd
 
-df = pd.read_csv("week_dates.csv")
+def getData(seasons, weeks):
+    df = pd.read_csv("week_dates.csv")
 
-df = df.groupby('season').tail(2).groupby('season').first()
+    df = df[df["season"].isin(seasons)]
+    df = df[df["week"].isin(weeks)]
 
-links = []
-base = "https://www.teamrankings.com"
+    links = []
+    base = "https://www.teamrankings.com"
 
-# getting links from file
-with io.open("data_urls.html", "r") as a:
-    html = soup(a.read(), "html.parser")
-    for i in html.find_all('a'):
-        links.append(base + i.get('href', '/'))
+    # getting links from file
+    with io.open("data_urls.html", "r") as a:
+        html = soup(a.read(), "html.parser")
+        for i in html.find_all('a'):
+            links.append(base + i.get('href', '/'))
 
-endDates = df["end"].tolist()
-statList = pd.DataFrame(data=None, columns=["season", "week", "team", 'stat', 'data'])
+    endDates = df["end"].tolist()
+    statList = pd.DataFrame(data=None, columns=["season", "week", "team", 'stat', 'data'])
 
-# i = 0
-# j = len(endDates) * len(links)
+    def getstuff(link, endDate, statList):
+        try:
+            statDf = pd.read_html(link + "?date=" + endDate)[0]
+            statDf = statDf[[statDf.columns[1], statDf.columns[2]]]
 
-def getstuff(link, endDate):
-    try:
-        statDf = pd.read_html(link + "?date=" + endDate)[0]
-        statDf = statDf[[statDf.columns[1], statDf.columns[2]]]
+            statDf = statDf.rename(columns={statDf.columns.to_list()[-1]: "data", "Team": "team"})
+            temp2 = df[df['end'] == endDate]
+            season = temp2.index[0]
+            week = temp2.iloc[0]['week']
+            stat = str(link).split("/")[-1]
 
-        statDf = statDf.rename(columns={statDf.columns.to_list()[-1]: "data", "Team": "team"})
-        temp2 = df[df['end'] == endDate]
-        season = temp2.index[0]
-        week = temp2.iloc[0]['week']
-        stat = str(link).split("/")[-1]
+            statDf.insert(0, column="season", value=([season] * len(statDf)), )
+            statDf.insert(1, column="week", value=([week] * len(statDf)))
+            statDf.insert(2, column="stat", value=([stat] * len(statDf)))
 
-        global statList
+            statList = pd.concat([statList, statDf], ignore_index=True)
+            return statList
+        except Exception as e:
+            print(str(e))
+            return statList
 
-        statDf.insert(0, column="season", value=([season] * len(statDf)), )
-        statDf.insert(1, column="week", value=([week] * len(statDf)))
-        statDf.insert(2, column="stat", value=([stat] * len(statDf)))
+    iterations = 1
 
-        statList = pd.concat([statList, statDf], ignore_index=True)
-    except Exception as e:
-        print(str(e))
-        statList.to_csv("data.csv")
+    for endDate in endDates:
+        i = 0
+        j = len(links)
+        for link in links:
+            print(str(i) + "/" + str(j) + " - iteration " + str(iterations) + " of " + str(len(endDates)))
+            i = i + 1
+            statList = getstuff(link, endDate, statList)
+        # statList.to_csv("yearly_data/stats/data_" + str(int(endDate.split("-")[0]) - 1) + ".csv")
+        iterations = iterations + 1
 
-iterations = 1
+    return statList
 
-for endDate in endDates:
-    i = 0
-    j = len(links)
-    for link in links:
-        print(str(i) + "/" + str(j) + " - iteration " + str(iterations) + " of " + str(len(endDates)))
-        i = i + 1
-        getstuff(link, endDate)
-    statList.to_csv("yearly_data/stats/data_" + str(int(endDate.split("-")[0]) - 1) + ".csv")
-    statList = statList.iloc[0:0]
-    iterations = iterations + 1
-
-print("done!")
+print(getData([2023], [1]))
 #statList.to_csv("data.csv")
