bbe8d3922e27cf0cf093ec0e673c2ac59678b04e
hairyex5000 <87248345+hairyex5000@users.noreply.github.com>
2024-05-31 13:52:39 -0700

    Added KNN imputer to gui

--- Product/gui_sm.py
+++ Product/gui_sm.py
@@ -4,6 +4,39 @@ import tensorflow as tf
 from tensorflow import keras
 import numpy as np
 import os
+from sklearn.impute import KNNImputer
+    
+def predict(df: pd.DataFrame):
+    df = df.set_index(["season", "team"])
+    temp = df.index
+    temp2 = df.columns
+    df = pd.DataFrame(columns=temp2, index=temp, data=KNNImputer(n_neighbors=5).fit_transform(df.to_numpy()))
+    df = df.reset_index()
+
+    display = pd.DataFrame(df["team"])
+
+    df = df.drop(columns=["season"])
+
+    answers = df["percent"].to_list()
+    df = df.drop(columns=["percent", "team"])
+
+    model = tf.keras.models.load_model("v1.keras")
+
+    stuff = model.predict(df.to_numpy()).flatten()
+    stuff = pd.Series(stuff).map(lambda x: round(x, 4)).to_numpy()
+    pm = []
+
+    for i in range(len(stuff)):
+        pm.append(str(round(abs(stuff[i] - answers[i]), 4)))
+
+    display.insert(len(display.columns), "Predictions", stuff)
+    display.insert(len(display.columns), "Answers", answers)
+    display.insert(len(display.columns), "Difference", pm)
+
+
+    display = display.set_index('team')
+
+    sl.table(display)
 
 sl.write("""
 # NFL Season Record Predictor
@@ -18,11 +51,11 @@ if choice == "Year":
         year = int(year)
         if year < 2003 or year > 2023:
             sl.error("Enter a year between 2003 and 2023")
-        
     except:
         sl.error("Please provide a valid type")
-    df = pd.read_csv("temp_onecopy.csv")
+    
     df = df[df["season"] == year]
+    predict(df)
 
 else:
     file = sl.file_uploader(label="Upload a CSV")
@@ -30,30 +63,4 @@ else:
         df = pd.read_csv(file)
     else:
         sl.write("Sample:")
-    
-    
-
-display = pd.DataFrame(df["team"])
-
-df = df.drop(columns=["season"])
-
-answers = df["percent"].to_list()
-df = df.drop(columns=["percent", "team"])
-
-model = tf.keras.models.load_model("v1.keras")
-model.load_weights("v1.h5")
-
-stuff = model.predict(df.to_numpy()).flatten()
-stuff = pd.Series(stuff).map(lambda x: round(x, 4)).to_numpy()
-pm = []
-
-for i in range(len(stuff)):
-    pm.append(str(round(abs(stuff[i] - answers[i]), 4)))
-
-display.insert(len(display.columns), "Predictions", stuff)
-display.insert(len(display.columns), "Answers", answers)
-display.insert(len(display.columns), "Difference", pm)
-
-display = display.set_index('team')
-
-sl.table(display)
+    predict(df)
